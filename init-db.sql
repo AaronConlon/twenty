3018a53658591b7c5278b4e2e48e110fd95dad67
Developer27149 <564151947@qq.com>
2025-06-13 16:31:43 +0800

    chore: update docker compose

---
+++ init-db.sql
@@ -0,0 +1,10 @@
+-- PostgreSQL initialization script
+-- This script will be executed when the container starts for the first time
+
+CREATE DATABASE "default";
+CREATE DATABASE test;
+
+-- Optional: Create additional users if needed
+-- CREATE USER app_user WITH PASSWORD 'app_password';
+-- GRANT ALL PRIVILEGES ON DATABASE "default" TO app_user;
+-- GRANT ALL PRIVILEGES ON DATABASE test TO app_user; 
